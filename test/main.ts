daff235fce166a18722fe05272cbbd76d1f872c3
Grant Fortington <gwfortington@hotmail.com>
2024-07-29 12:17:10 +0930

    Re-factored

--- test/main.ts
+++ test/main.ts
@@ -17,67 +17,65 @@ describe('main', () => {
     await pg.transaction(async (query) => {
       await query(
         `CREATE TABLE ${table} (` +
-          'id serial,' +
+          'id serial,' + // serial is shorthand for creating an autoincrementing integer
           'name varchar(30) NOT NULL,' +
-          'description varchar(255),' +
-          '_char char(10),' +
-          '_varchar varchar(80),' +
+          'description text,' + // text w/chk constraint is equiv. to varchar(255)
           '_text text,' +
           '_smallint smallint,' +
           '_integer integer,' +
           '_bigint bigint,' +
-          '_decimal decimal(6, 2),' +
+          '_decimal decimal(10, 2),' + // decimal is alias for numeric
           '_date date,' +
-          '_time time(0),' +
-          '_timetz timetz(1),' +
-          '_datetime timestamp(2),' +
-          '_datetimetz timestamptz(3),' +
+          '_time time,' +
+          '_datetime timestamp,' +
+          '_datetimetz timestamptz,' +
           '_boolean boolean,' +
-          'parent_id integer,' +
-          `CONSTRAINT ${table}_pk PRIMARY KEY (id),` +
-          `CONSTRAINT ${table}_uk UNIQUE (name),` +
-          `CONSTRAINT ${table}_fk FOREIGN KEY (parent_id) REFERENCES ${table} (id)` +
+          `CONSTRAINT ${table}_id_pk PRIMARY KEY (id),` +
+          `CONSTRAINT ${table}_name_uk UNIQUE (name),` +
+          `CONSTRAINT ${table}_description_chk CHECK (char_length(description) <= 255)` +
           ')',
       );
       await query(
-        `INSERT INTO ${table} (name, description, _char) VALUES ('char', 'fixed-length, blank-padded', 'n=10')`,
+        `INSERT INTO ${table} (name, description) VALUES ` +
+          "('text', 'variable unlimited length'), " +
+          "('smallint', 'small-range integer'), " +
+          "('integer', 'typical choice for integer'), " +
+          "('bigint', 'large-range integer'), " +
+          "('decimal', 'user-specified precision, exact'), " +
+          "('date', 'date (no time of day)'), " +
+          "('time', 'time of day (no date)'), " +
+          "('datetime', 'both date and time (no time zone)'), " +
+          "('datetimetz', 'both date and time, with time zone'), " +
+          "('boolean', 'state of true or false')",
       );
       await query(
-        `INSERT INTO ${table} (name, description, _varchar) VALUES ('varchar', 'variable-length with limit', 'n=80')`,
+        `UPDATE ${table} SET _text = 'Up to 65,535 bytes' WHERE name = 'text'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _text) VALUES ('text', 'variable unlimited length', 'n=any')`,
+        `UPDATE ${table} SET _smallint = 32767 WHERE name = 'smallint'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _smallint) VALUES ('smallint', 'small-range integer', 32767)`,
+        `UPDATE ${table} SET _integer = 2147483647 WHERE name = 'integer'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _integer) VALUES ('integer', 'typical choice for integer', 2147483647)`,
+        `UPDATE ${table} SET _bigint = 900719925474099 WHERE name = 'bigint'`,
+      ); // using js MAX_SAFE_INTEGER in lieu of pg max. 9223372036854775807
+      await query(
+        `UPDATE ${table} SET _decimal = 99999999.99 WHERE name = 'decimal'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _bigint) VALUES ('bigint', 'large-range integer', 9223372036854775807)`,
+        `UPDATE ${table} SET _date = '1999-01-08' WHERE name = 'date'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _decimal) VALUES ('decimal', 'user-specified precision, exact', 9999.99)`,
+        `UPDATE ${table} SET _time = '04:05:06.789' WHERE name = 'time'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _date) VALUES ('date', 'date (no time of day)', '1999-01-08')`,
+        `UPDATE ${table} SET _datetime = '1999-01-08T04:05:06.789' WHERE name = 'datetime'`,
       );
       await query(
-        `INSERT INTO ${table} (name, description, _time) VALUES ('time', 'time of day (no date)', '04:05:06.789')`,
+        `UPDATE ${table} SET _datetimetz = '1999-01-08T04:05:06.789' WHERE name = 'datetimetz'`,
       );
-      await query(
-        `INSERT INTO ${table} (name, description, _timetz) VALUES ('timetz', 'time of day (no date), with time zone', '04:05:06.789')`,
-      );
-      await query(
-        `INSERT INTO ${table} (name, description, _datetime) VALUES ('datetime', 'both date and time (no time zone)', '1999-01-08T04:05:06.789')`,
-      );
-      await query(
-        `INSERT INTO ${table} (name, description, _datetimetz) VALUES ('datetimetz', 'both date and time, with time zone', '1999-01-08T04:05:06.789')`,
-      );
-      await query(
-        `INSERT INTO ${table} (name, description, _boolean) VALUES ('boolean', 'state of true or false', true)`,
-      );
+      await query(`UPDATE ${table} SET _boolean = true WHERE name = 'boolean'`);
     });
     pg.types.setTypeParser(pg.types.builtins.INT8, (value) => parseInt(value)); // bigint
     pg.types.setTypeParser(pg.types.builtins.NUMERIC, (value) =>
@@ -88,10 +86,10 @@ describe('main', () => {
     pg.types.setTypeParser(pg.types.builtins.TIMESTAMP, datetimeParser); // datetime
     pg.types.setTypeParser(pg.types.builtins.TIMESTAMPTZ, datetimeParser); // datetimetz
   });
-  it('should select 13 rows', async () => {
+  it('should select 10 rows', async () => {
     const result = await pg.query(`SELECT * FROM ${table}`);
     console.log(result.rows);
-    assert.equal(result.rowCount, 13);
+    assert.equal(result.rowCount, 10);
   });
   after(async () => {
     await pg.query(`DROP TABLE ${table}`);
